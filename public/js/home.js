1e824d00924f9495dd7746091f66578a69d04051
GauravMG <gauravmittal17396@gmail.com>
2025-05-15 13:37:29 +0530

    new dev - dynamic banner on web home page cover banners

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,39 @@
 $(document).ready(function () {
+    fetchBanners()
     fetchCuratedBestsellers()
 })
+
+async function fetchBanners() {
+    await postAPICall({
+        endPoint: "/banner/list",
+        payload: JSON.stringify({
+            "filter": {},
+            "range": {
+                all: true
+            },
+            "sort": [{
+                "orderBy": "sequenceNumber",
+                "orderDir": "asc"
+            }]
+        }),
+        callbackComplete: () => { },
+        callbackSuccess: (response) => {
+            const { success, message, data } = response
+
+            if (success) {
+                let html = ""
+
+                for (let i = 0; i < data?.length; i++) {
+                    html += `<div class="carousel-item${i === 0 ? " active" : ""}">
+                        <img src="${data[i].mediaUrl}" class="d-block w-100" alt="${data[i].name}">
+                    </div>`
+                }
+
+                document.getElementById("coverBannerContainer").innerHTML = html
+            }
+        }
+    })
+}
 
 async function fetchCuratedBestsellers() {
     await postAPICall({

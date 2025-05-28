5864b475b9b98da00ec163aa230e7c84169dc1a9
GauravMG <gauravmittal17396@gmail.com>
2025-05-28 15:17:23 +0530

    modify - ui of attributes on category product list page filter

--- public/js/category.js
+++ public/js/category.js
@@ -268,8 +268,8 @@ async function renderAttributeFilters() {
                                 <h6>${attr.name}</h6>
                                 <span><i class="fa-solid fa-caret-down"></i></span>
                             </div>
-                            <div class="attribute-input d-none">${inputHTML}</div>
-                            <ul></ul>
+                            <!-- <div class="attribute-input d-none">${inputHTML}</div> -->
+                            <ul>${inputHTML}</ul>
                         `;
 
                         container.appendChild(wrapper);

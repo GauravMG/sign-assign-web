45cdcb1ba4e723619ce857639e79bdc04e4c7d63
GauravMG <gauravmittal17396@gmail.com>
2025-05-28 21:53:57 +0530

    user profile redirect to dashboard

--- public/js/common.js
+++ public/js/common.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
         const token = localStorage.getItem('jwtTokenUser');
         const user = localStorage.getItem('userDataUser');
         const encodedUser = encodeURIComponent(user);
-        const userDashboardLink = `${baseUrlUserDashboard}user-dashboard?token=${token}&user=${encodedUser}`;
+        const userDashboardLink = `${baseUrlUserDashboard}user-dashboard/dashboard?token=${token}&user=${encodedUser}`;
 
         htmlNavbarAuthOptionsContainer = `<a style="cursor: pointer;" href="${userDashboardLink}" class="profile-icon" title="Profile">
             <i class="fi fi-rs-user-gear"></i>

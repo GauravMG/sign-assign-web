7d2ee07a22f2fa6329844193b589249ee3794557
Manish rawat <manish.rawat@essencesoftwares.com>
2025-06-05 01:16:28 +0530

    feat: blog listing page added

--- app/Views/web/blogs.php
+++ app/Views/web/blogs.php
@@ -1,11 +1,155 @@
 <?= $this->extend('web/web_template'); ?>
 
 <?= $this->section('pageStyles'); ?>
-<link rel="stylesheet" href="<?= base_url('css/blogs.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('css/blogs.css') . '?t=' . time(); ?>">
 <?= $this->endSection(); ?>
 
 <?= $this->section('content'); ?>
-<h1>Blogs page</h1>
+<div class="subheader-area">
+    <div class="container-fluid">
+    </div>
+</div>
+<div class="container-fluid py-5">
+    <div class="blog-area">
+        <div class="inner-card">
+            <div class="p-3 m-0">
+                <video class="w-100 rounded" muted="" loop="" playsinline="">
+                    <source src="http://3.109.198.252/api/uploads/big-buck-bunny-720p-1mb-mp4-1749060979117-241675962.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div class="px-3 mt-0">
+                <h6>by signassi | Mon, Jun 02, 2025 | Signage</h6>
+                <h5>Design, Customize, Promote: How Sign Assign Is Transforming DIY Marketing for Businesses</h5>
+                <p>In the fast-paced world of business
+                    marketing, visibility i...</p>
+                <a href="/blogs/design-customize-promote-how-sign-assign-is-transforming-diy-marketing-for-businesses-5">Read More <span><svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="inner-card">
+            <div class="p-3 m-0">
+                <video class="w-100 rounded" muted="" loop="" playsinline="">
+                    <source src="http://3.109.198.252/api/uploads/big-buck-bunny-720p-1mb-mp4-1749060979117-241675962.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div class="px-3 mt-0">
+                <h6>by signassi | Mon, Jun 02, 2025 | Signage</h6>
+                <h5>Design, Customize, Promote: How Sign Assign Is Transforming DIY Marketing for Businesses</h5>
+                <p>In the fast-paced world of business
+                    marketing, visibility i...</p>
+                <a href="/blogs/design-customize-promote-how-sign-assign-is-transforming-diy-marketing-for-businesses-5">Read More <span><svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="inner-card">
+            <div class="p-3 m-0">
+                <img src="http://3.109.198.252/api/uploads/download-jpeg-1748865714876-973428608.jpeg" alt="" class="w-100 rounded">
+            </div>
+            <div class="px-3 mt-0">
+                <h6>by signassi | Mon, Jun 02, 2025 | Signage</h6>
+                <h5>Design, Customize, Promote: How Sign Assign Is Transforming DIY Marketing for Businesses</h5>
+                <p>In the fast-paced world of business
+                    marketing, visibility i...</p>
+                <a href="/blogs/design-customize-promote-how-sign-assign-is-transforming-diy-marketing-for-businesses-5">Read More <span><svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="inner-card">
+            <div class="p-3 m-0">
+                <video class="w-100 rounded" muted="" loop="" playsinline="">
+                    <source src="http://3.109.198.252/api/uploads/big-buck-bunny-720p-1mb-mp4-1749060979117-241675962.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div class="px-3 mt-0">
+                <h6>by signassi | Mon, Jun 02, 2025 | Signage</h6>
+                <h5>Design, Customize, Promote: How Sign Assign Is Transforming DIY Marketing for Businesses</h5>
+                <p>In the fast-paced world of business
+                    marketing, visibility i...</p>
+                <a href="/blogs/design-customize-promote-how-sign-assign-is-transforming-diy-marketing-for-businesses-5">Read More <span><svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="inner-card">
+            <div class="p-3 m-0">
+                <video class="w-100 rounded" muted="" loop="" playsinline="">
+                    <source src="http://3.109.198.252/api/uploads/big-buck-bunny-720p-1mb-mp4-1749060979117-241675962.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+            <div class="px-3 mt-0">
+                <h6>by signassi | Mon, Jun 02, 2025 | Signage</h6>
+                <h5>Design, Customize, Promote: How Sign Assign Is Transforming DIY Marketing for Businesses</h5>
+                <p>In the fast-paced world of business
+                    marketing, visibility i...</p>
+                <a href="/blogs/design-customize-promote-how-sign-assign-is-transforming-diy-marketing-for-businesses-5">Read More <span><svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+        <div class="inner-card">
+            <div class="p-3 m-0">
+                <img src="http://3.109.198.252/api/uploads/download-jpeg-1748865714876-973428608.jpeg" alt="" class="w-100 rounded">
+            </div>
+            <div class="px-3 mt-0">
+                <h6>by signassi | Mon, Jun 02, 2025 | Signage</h6>
+                <h5>Design, Customize, Promote: How Sign Assign Is Transforming DIY Marketing for Businesses</h5>
+                <p>In the fast-paced world of business
+                    marketing, visibility i...</p>
+                <a href="/blogs/design-customize-promote-how-sign-assign-is-transforming-diy-marketing-for-businesses-5">Read More <span><svg class="svg-inline--fa fa-arrow-right-long" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-right-long" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
+                            <path fill="currentColor" d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128z"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="form-area py-5">
+    <div class="container-fluid">
+        <div class="form-inner-area">
+            <div class="text-area">
+                <h4 class="mb-3">Trade Partners Can Get Up to 30% Off!</h4>
+                <p>
+                    We are offering exclusive discounts for trade partners. All you need to do is provide us with
+                    your name and email, we'll
+                    ask for some further details and upon confirmation you can enjoy
+                    up to 30% discount on your orders!
+                </p>
+            </div>
+            <div class="form-card">
+                <form action="#">
+                    <div class="form-group">
+                        <input type="name" placeholder="First Name" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="name" placeholder="Last Name" class="form-control" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="email" placeholder="Email Address" class="form-control" required>
+                    </div>
+                    <a href="#">Submit Now</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 <?= $this->endSection(); ?>
 
 <?= $this->section('pageScripts'); ?>

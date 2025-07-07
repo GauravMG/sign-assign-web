401d09a042f44d608126acfd0bbca5ba1a1e2f5a
GauravMG <gauravmittal17396@gmail.com>
2025-07-07 11:31:31 +0530

    fix: web: common-out service list from services and service-details page

--- app/Views/web/services.php
+++ app/Views/web/services.php
@@ -10,28 +10,7 @@
     </div>
 </div>
 
-<div class="container-fluid py-5">
-    <div class="grid-inner">
-        <div class="inner" onclick="window.location.href = '/services/turnkey-project-management';">
-            <h5>Turnkey Project Management</h5>
-        </div>
-        <div class="inner" onclick="window.location.href = '/services/sign-services-and-installation';">
-            <h5>Sign Services & Installation</h5>
-        </div>
-        <div class="inner" onclick="window.location.href = '/services/permitting';">
-            <h5>Permitting</h5>
-        </div>
-        <div class="inner" onclick="window.location.href = '/services/code-review-and-compliance';">
-            <h5>Code Review and Compliance</h5>
-        </div>
-        <div class="inner" onclick="window.location.href = '/services/sign-engineering';">
-            <h5>Sign Engineering</h5>
-        </div>
-        <div class="inner" onclick="window.location.href = '/services/sign-repairs';">
-            <h5>Sign Repairs</h5>
-        </div>
-    </div>
-</div>
+<?= $this->include('web/partials/services-list'); ?>
 
 <div class="business-growth-area py-5">
     <div class="container-fluid">
